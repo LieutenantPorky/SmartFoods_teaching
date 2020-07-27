b424613ca22c6af6e8d2b72e99771b1e6466e89b
lieutenantPorky <jacopo@siniscalco.eu>
2020-07-27 19:20:03 +0100

    added comments for lesson 2

--- main.py
+++ main.py
@@ -1,13 +1,32 @@
-from flask import Flask, render_template
+from flask import Flask, render_template # Flask is a python module, so it needs to be imported at the start
+
+# Quickly setup the environment - we're telling Flask that it should run as a server, as well as giving it the location
+# of static files (files that need to be fetched separately), and telling it to enable debugging
 
 app = Flask(__name__, static_folder="./templates", static_url_path="/templates")
 app.debug=True
 
+# Here we're actually telling Flask what to do when a user connects. The @app.route indicates what sub-url the user should go
+# to in order for the function that follows to be called - for example, if we have a server address that's "smartfoods.com",
+# and have
+# @app.route("/fridge")
+# def showFridge():
+# ...
+#
+#
+# Then showFridge is called whenever a user visits "smartfoods.com/fridge"
+#
+# In this particular case, we're just going to add the function at the root of the url
 @app.route("/")
 def homePage():
+    #This will be printed to the console once a user connects
     print("User connected to the homepage")
+
+    #This will return the html code that we've included in our template
     return render_template("template_1.html")
 
+# This statement is only true if the script is actually being launched separately, and not as a sub_process. This ensures we don't
+# accidentally start the Flask server in a situation where it may be insecure
 
 if __name__ == "__main__":
     app.run()
